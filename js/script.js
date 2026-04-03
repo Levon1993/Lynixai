f3bdb40b2058b83e6be82b0df62998524056fb52
Levon1993 <styleitup.info@yahoo.com>
2026-04-03 19:07:31 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,14 +2,3 @@ document.getElementById("contactForm")?.addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Nachricht gesendet (Demo)");
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  if (!localStorage.getItem("cookiesAccepted")) {
-    document.getElementById("cookieBanner").style.display = "flex";
-  }
-
-  document.getElementById("acceptCookies").addEventListener("click", function() {
-    localStorage.setItem("cookiesAccepted", "true");
-    document.getElementById("cookieBanner").style.display = "none";
-  });
-});
