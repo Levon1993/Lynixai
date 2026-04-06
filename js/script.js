fc0e74e6b3835a6b17fe297336a70c72a91ea7ca
Levon1993 <styleitup.info@yahoo.com>
2026-04-06 14:30:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ document.getElementById("contactForm")?.addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Nachricht gesendet (Demo)");
 });
+
+button.addEventListener("click", function () {
+  localStorage.setItem("cookiesAccepted", "true");
+  banner.classList.remove("cookie-show");    // Animation raus
+  banner.classList.add("cookie-hidden");     // wieder ausblenden
+});
